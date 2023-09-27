b871fdfc12b60b223e7c2691e6837f71e46bb89c
Pratham <mishrapratham.codeit@gmail.com>
2023-09-27 18:28:47 +0530

    (stable)

--- core/nucleus.py
+++ core/nucleus.py
@@ -1,5 +1,14 @@
 
 class Tree:
-    def __init__(self, d_list: list, f_list: list) -> None:
-        self.d_list = d_list
+    def __init__(self, f_list: list) -> None:
         self.f_list = f_list
+
+    def __display__(self, name):
+        print()
+        for file in self.f_list:
+            print(file[file.find(name.split('/')[-1]):])
+
+
+def cloud(mlist: list, root: str) -> None:
+    hell = Tree(mlist)
+    hell.__display__(root)
