6d7c4d817399497e9a6c99aa4269de3e3618ec9f
Pratham <mishrapratham.codeit@gmail.com>
2023-09-27 18:28:25 +0530

    added the ignore logic

---
+++ core/ignore.py
@@ -0,0 +1,17 @@
+'''
+Put the names of all the folders you want to ignore while making your directory' tree in the `ignor` list.
+
+Note: Do not try to change the dimension or the name of the list. Use "/<folder-name>" convention for writing your folders. For example:
+
+ignor = [
+    "/.git",
+    "/__pycache__"
+]
+
+Also remember to give '/' in path rather than '\'.
+'''
+
+ignor = [
+    "/.git",
+    "/__pycache__"
+]
